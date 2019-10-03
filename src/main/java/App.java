b285aad7fb10606509b8970e68c3228ae853d028
Zedory <zied.mustapha@epfl.ch>
2019-10-03 22:50:23 +0200

    Exo 1.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,76 +1,85 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+class BankAccount {
+    public enum CurrencyType { DOLLAR, EURO, POUND };
+
+    public BankAccount(CurrencyType currencyType, double amount) {
+        this.currencyType = currencyType;
+        this.amount = amount;
+    }
+
+    private CurrencyType currencyType;
+    private double amount;
+
+    public boolean add(CurrencyType addedType, double addedAmount) {
+        double inCurrency = 0;
+        if(currencyType == addedType) {
+            inCurrency = addedAmount;
+        }
+        else if(currencyType == CurrencyType.DOLLAR && addedType == CurrencyType.EURO) {
+            inCurrency = 1.15 * addedAmount;
+        }
+        else if(currencyType == CurrencyType.DOLLAR && addedType == CurrencyType.POUND) {
+            inCurrency = 1.31 * addedAmount;
+        }
+        else if(currencyType == CurrencyType.EURO && addedType == CurrencyType.DOLLAR) {
+            inCurrency = 0.87 * addedAmount;
+        }
+        else if(currencyType == CurrencyType.EURO && addedType == CurrencyType.POUND) {
+            inCurrency = 1.14 * addedAmount;
+        }
+        else if(currencyType == CurrencyType.POUND && addedType == CurrencyType.DOLLAR) {
+            inCurrency = 0.76 * addedAmount;
+        }
+        else if(currencyType == CurrencyType.POUND && addedType == CurrencyType.EURO) {
+            inCurrency = 0.88 * addedAmount;
+        }
+        amount += inCurrency;
+        return true;
+    }
+
+    public boolean remove(CurrencyType removedType, double removedAmount) {
+        double inCurrency = 0;
+        if(currencyType == removedType) {
+            inCurrency = removedAmount;
+        }
+        else if(currencyType == CurrencyType.DOLLAR && removedType == CurrencyType.EURO) {
+            inCurrency = 1.15 * removedAmount;
+        }
+        else if(currencyType == CurrencyType.DOLLAR && removedType == CurrencyType.POUND) {
+            inCurrency = 1.31 * removedAmount;
+        }
+        else if(currencyType == CurrencyType.EURO && removedType == CurrencyType.DOLLAR) {
+            inCurrency = 0.87 * removedAmount;
+        }
+        else if(currencyType == CurrencyType.EURO && removedType == CurrencyType.POUND) {
+            inCurrency = 1.14 * removedAmount;
+        }
+        else if(currencyType == CurrencyType.POUND && removedType == CurrencyType.DOLLAR) {
+            inCurrency = 0.76 * removedAmount;
+        }
+        else if(currencyType == CurrencyType.POUND && removedType == CurrencyType.EURO) {
+            inCurrency = 0.88 * removedAmount;
+        }
+        if(inCurrency > amount) {
+            return false;
+        }
+        amount -= inCurrency;
+        return true;
+    }
+
+    public CurrencyType getCurrencyType() {
+        return currencyType;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
 
 public class App {
-    public static void main(String[] args) throws FileNotFoundException {
-        compute();
-    }
-
-    public static List<Double> compute() throws FileNotFoundException {
-
-        /* Find the file and create a scanner */
-        File file = new File("data");
-        Scanner scanner = new Scanner(file);
-
-        List<Double> numbers = inputReading(scanner); // read input
-        double mean = meanComputing(numbers); // compute the mean
-        double std = standardDev(numbers, mean); // compute the standard deviation
-        List<Double> normalized = normalization(numbers, mean, std); // compute the normalization
-        return outputSpreading(normalized, "output"); // output the normalization in a file and return the normalization
-    }
-
-    private static List<Double> inputReading(Scanner scanner){
-        List<Double> numbers = new ArrayList<>();
-        while (scanner.hasNextDouble()) {
-            double number = scanner.nextDouble();
-            numbers.add(number);
-        }
-        scanner.close();
-        return numbers;
-    }
-
-    private static double meanComputing(List<Double> numbers){
-        double sum = 0;
-        for (double f : numbers) {
-            sum += f;
-        }
-        return sum / numbers.size();
-    }
-
-    private static double standardDev(List<Double> numbers, double mean){
-        double sumSquare = 0;
-        for (double f : numbers) {
-            double diff = f - mean;
-            sumSquare += diff * diff;
-        }
-        return Math.sqrt(sumSquare / numbers.size());
-    }
-
-    private static List<Double> normalization(List<Double> numbers, double mean, double std){
-        List<Double> normalized = new ArrayList<>();
-        for (double f : numbers) {
-            normalized.add((f - mean) / std);
-        }
-        return normalized;
-    }
-
-    private static List<Double> outputSpreading(List<Double> normalized, String fileName){
-        System.out.println(normalized);
-        try {
-            FileWriter fw = new FileWriter(fileName);
-            for (double n : normalized) {
-                fw.write(Double.toString(n));
-                fw.write("\n");
-            }
-            fw.close();
-        } catch (Exception e) {
-            System.out.println("Error writing output file");
-        }
-        System.out.println("Wrote output file.");
-        return normalized;
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(BankAccount.CurrencyType.EURO, 9001);
+        account.add(BankAccount.CurrencyType.DOLLAR, 100);
+        account.remove(BankAccount.CurrencyType.POUND, 10);
+        System.out.println("Balance: " + account.getAmount());
     }
 }
